4711e764ccd135da2ac00b22a4d3ade28165bcc4
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-01-08 16:14:16 +0100

    analýza nejak funguje - jdu delat branch na individualni vysledky

--- utils/print_png_eps_svg_fig.m
+++ utils/print_png_eps_svg_fig.m
@@ -1,7 +1,7 @@
-function print_png_eps_svg(name)
+function print_png_eps_svg_fig(name)
 
     print(name,'-depsc')
     print(name,'-dpng','-r250')
     print(name,'-dsvg')
-
+    savefig([name '.fig'])
 end
