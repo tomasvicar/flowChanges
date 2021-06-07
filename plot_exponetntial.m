afd191a0095fc61c1f16f0c4100338b96d1270ff
Tomáš Vičar <vicar@PC-SE4158-716.ubmi.feec.vutbr.cz>
2021-06-07 16:48:05 +0200

    hotovo

---
+++ plot_exponetntial.m
@@ -0,0 +1,124 @@
+clc;clear all;close all;
+
+
+
+% tau = 5; %Pa
+% 
+% 
+% % G = 18; %Pa
+% % eta = 80; %Pa
+% G = 70; %Pa
+% eta = 350; %Pa
+% 
+% 
+%  
+% 
+% 
+% 
+% t = -2:0.01:30;
+% 
+% 
+% 
+% gamma = tau/G * (1-exp(-G/eta*t));
+% gamma(t<0) = 0;
+% 
+% plot(t,gamma)
+% 
+% ylim([0,0.3])
+% xlim([-2,30])
+% 
+% print(['C:\Users\vicar\OneDrive - Vysoké učení technické v Brně\obrazek_shear_stress\prubehy' 'G' num2str(G) 'eta' num2str(eta)],'-depsc')
+% print(['C:\Users\vicar\OneDrive - Vysoké učení technické v Brně\obrazek_shear_stress\prubehy' 'G' num2str(G) 'eta' num2str(eta)],'-dsvg')
+
+
+
+
+% 
+% tau = 5; %Pa
+% 
+% 
+% G = 50; %Pa
+% eta = 300; %Pa
+% 
+% 
+% 
+% t = -2:0.01:30;
+% 
+% 
+% 
+% gamma = tau/G * (1-exp(-G/eta*t));
+% gamma(t<0) = 0;
+% 
+% plot(t,gamma)
+% 
+% ylim([0,0.11])
+% xlim([-2,30])
+% 
+% 
+% print('C:\Users\vicar\OneDrive - Vysoké učení technické v Brně\obrazek_shear_stress\bunky','-dsvg')
+% 
+% 
+% 
+
+
+
+
+
+
+
+% 
+% tau = 5; %Pa
+% 
+% 
+% G = 50; %Pa
+% eta = 300/7; %Pa
+% 
+% 
+% 
+% t = -2:0.01:30;
+% 
+% 
+% 
+% gamma = tau/G * (1-exp(-G/eta*t));
+% gamma(t<0) = 0;
+% 
+% plot(t,gamma)
+% 
+% ylim([0,0.11])
+% xlim([-2,30])
+% 
+% 
+% print('C:\Users\vicar\OneDrive - Vysoké učení technické v Brně\obrazek_shear_stress\prutok','-dsvg')
+
+
+
+
+
+
+% tau = 5; %Pa
+% 
+% 
+% G = 50; %Pa
+% eta = 300/10000; %Pa
+% 
+% 
+% 
+% t = -2:0.01:30;
+% 
+% 
+% 
+% gamma = tau/G * (1-exp(-G/eta*t));
+% gamma(t<0) = 0;
+% 
+% plot(t,gamma)
+% 
+% ylim([0,0.11])
+% xlim([-2,30])
+% 
+% 
+% print('C:\Users\vicar\OneDrive - Vysoké učení technické v Brně\obrazek_shear_stress\step','-dsvg')
+
+
+
+
+
